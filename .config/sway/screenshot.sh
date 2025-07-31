12842d026b639f26e0d10edd423b31988232159c
anlleguizamongu <anlleguizamongu@unal.edu.co>
2025-07-31 02:08:16 -0500

    update

--- .config/sway/screenshot.sh
+++ .config/sway/screenshot.sh
@@ -6,9 +6,9 @@ FILENAME="$DIR/ss$(date +'%d%m%y-%H%M%S').png"
 case $1 in
 	0)
 		# Capturar área seleccionada
-		AREA=$(slurp)
+		AREA=$(slurp -b "1b032977" -c "39ff14" -w 1)
 		if [ -z "$AREA" ]; then
-			notify-send -u normal "Captura cancelada" "No se seleccionó un área válida"
+			notify-send -u normal "Screenshot cancelled" "No valid area selected"
 			exit 1
 		fi
 
@@ -16,7 +16,7 @@ case $1 in
 
 		# Copiar al portapapeles
 		wl-copy < "$FILENAME"
-		notify-send -u low "Captura de pantalla - Área seleccionada" "$FILENAME"
+		notify-send -u low "Screenshot - Selected area" "$FILENAME"
 
 		;;	
 	1)	
@@ -25,9 +25,9 @@ case $1 in
 
 		# Copiar al portapapeles
 		wl-copy < "$FILENAME"
-		notify-send -u low "Captura de pantalla - Pantalla completa" "$FILENAME"
+		notify-send -u low "Screenshot - Full screen" "$FILENAME"
 		;;
 	*)
-		notify-send -u critical "Opcion no valida"
+		notify-send -u critical "Invalid option"
 		;;
 esac
